cdda59371f7ad4454feb9c92e8fc2b3d050923cb
nuriaktas <aktasnuri88@gmail.com>
2026-05-23 12:59:13 +0300

    Update Google Maps profile link

--- src/lib/external-links.ts
+++ src/lib/external-links.ts
@@ -1,5 +1,5 @@
 export const GOOGLE_MAPS_PROFILE_URL =
-  "https://www.google.com/maps/place/Diyetisyen+Ezgi+Evgin/@39.9669753,32.6332346,17z/data=!3m1!4b1!4m6!3m5!1s0x14d330d2f71d4659:0x83b8bf59458d8408!8m2!3d39.9669753!4d32.6358095!16s%2Fg%2F11dymr8nhs?entry=ttu&g_ep=EgoyMDI2MDQwMS4wIKXMDSoASAFQAw%3D%3D";
+  "https://www.google.com/maps/place/Diyetisyen+Ezgi+Evgin/@39.966974,32.6133851,14z/data=!4m17!1m10!4m9!1m4!2m2!1d32.7155712!2d39.8819328!4e1!1m3!2m2!1d32.6358095!2d39.9669753!3m5!1s0x14d330d2f71d4659:0x83b8bf59458d8408!8m2!3d39.9669753!4d32.6358095!16s%2Fg%2F11dymr8nhs?entry=ttu&g_ep=EgoyMDI2MDUyMC4wIKXMDSoASAFQAw%3D%3D";
 
 export const GOOGLE_MAPS_DIRECTIONS_URL =
   "https://www.google.com/maps/dir/?api=1&destination=39.9669753,32.6358095&destination_place_id=ChIJWUYd99Iw0xQRDITdRlm_uIM";
